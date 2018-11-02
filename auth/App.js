e66729b9b3264136938f12018906ce7e5720fe7e
Gerald2030 <gerald2030@yahoo.com>
2018-11-02 23:32:14 +0500

    changes

--- auth/App.js
+++ auth/App.js
@@ -1,11 +1,17 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import firebase from 'firebase'
-import {View} from 'react-native';
-import {Header} from './src/components/common'
-                            
+import { View, Text } from 'react-native';
+import { Header, Button, Spinner } from './src/components/common'
+import LoginForm from './src/components/LoginForm'
+
+
 export default class App extends Component {
-  componentDidMount(){
-    firebase.initializeApp( {
+  state = {
+    loggedIn: null
+  };
+
+  componentDidMount() {
+    firebase.initializeApp({
       apiKey: "AIzaSyBxrC7wDYReuJ-p1a2YdDa5lZrsHpu6N04",
       authDomain: "authentication-17.firebaseapp.com",
       databaseURL: "https://authentication-17.firebaseio.com",
@@ -13,13 +19,37 @@ export default class App extends Component {
       storageBucket: "authentication-17.appspot.com",
       messagingSenderId: "722094038539"
     })
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      }
+      else {
+        this.setState({ loggedIn: false });
+
+      }
+    })
   }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return <Button> Log Out </Button>;
+      case false:
+        return <LoginForm />;
+      default:
+        return <Spinner size="large" />
+    }
+
+
+  }
+
   render() {
     return (
       <View >
-        <Header headerText="Authentication" />
-        <Text> An App! </Text>
-    
+        <Header headertext="Authentication" />
+        {this.renderContent()}
+        <LoginForm />
+        <Button > logout </Button>
       </View>
     );
   }
