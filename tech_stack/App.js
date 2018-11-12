5fff25a561c5164eb1a5913ee1b4122b91b25bda
Gerald2030 <gerald2030@yahoo.com>
2018-11-12 09:52:37 +0500

    S

--- tech_stack/App.js
+++ tech_stack/App.js
@@ -4,7 +4,7 @@ import {Provider}from 'react-redux';
 import {createStore}from 'redux';
 import reducers from './src/reducers';
 import {Header} from './src/components/common'
-
+import LibraryList from './src/components/LibraryList'
 
 export default class App extends Component {
   render() {
@@ -12,6 +12,7 @@ export default class App extends Component {
       <Provider store={createStore(reducers)}>
       <View >
         <Header headerText="Tech Stack" />
+        <LibraryList/>
       </View>
       </Provider>
     );
