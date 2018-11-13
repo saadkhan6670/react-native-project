0e5f0455068adf613d7a095153190a91c69e3bfe
saadkhan6670 <saad.khan6670@gmail.com>
2018-11-13 11:58:06 +0500

    changes

--- Tech-stack/App.js
+++ Tech-stack/App.js
@@ -1,21 +1,20 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import {View} from 'react-native';
+import {Provider}from 'react-redux';
+import {createStore}from 'redux';
+import reducers from './src/reducers';
+import {Header} from './src/components/common'
+import LibraryList from './src/components/LibraryList'
 
-export default class App extends React.Component {
+export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <Provider store={createStore(reducers)}>
+      <View >
+        <Header headerText="Tech Stack" />
+        <LibraryList/>
       </View>
+      </Provider>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
